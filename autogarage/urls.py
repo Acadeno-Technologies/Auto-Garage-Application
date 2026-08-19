77fe64d1f4abdc798a0d8a8ef2e43a240fb35a2a
Arathy <arathy@acadeno.in>
2026-08-19 16:31:31 +0530

    feat: configure project-level URL patterns and static/media file serving paths

--- autogarage/urls.py
+++ autogarage/urls.py
@@ -4,9 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
+import os
+
+static_dir = settings.STATIC_ROOT if (os.path.exists(settings.STATIC_ROOT) and os.listdir(settings.STATIC_ROOT)) else (settings.BASE_DIR / 'core' / 'static')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': static_dir}),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=static_dir) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
